878576a0744e4e0d924dc93c23ba918cccf61d33
masakihongo <hongo@egaoinc.jp>
2013-12-17 14:15:25 +0900

    初期設定

--- config.rb
+++ config.rb
@@ -1,17 +1,20 @@
 # Require any additional compass plugins here.
-
+cache = false
+asset_cache_buster :none
 # Set this to the root of your project when deployed:
+
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
+sass_dir = "_sass"
+images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
+# output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = false
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
